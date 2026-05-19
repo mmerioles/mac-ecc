6bcf81fc18ff96a061428d5931a1a6dd2dbda7cd
mmerioles <matthewmerioles@yahoo.com>
2026-05-19 11:58:59 -0700

    adding test cases template

--- tests/test_modulus.py
+++ tests/test_modulus.py
@@ -1,5 +1,11 @@
-def test_modular_multiplication():
-    assert True
+import pytest
+
+@pytest.mark.parametrize("a,b,p,expected", [
+    (10, 20, 100, 1000),
+    (1, 2, 100, 100)
+])
+def test_modular_multiplication(a, b, p, expected):
+    assert a * p == expected
 
 def test_modular_exponentiation():
     assert True
