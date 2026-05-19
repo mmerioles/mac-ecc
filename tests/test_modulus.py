0acc319f3f543a4e283f7a8f59ab8e120548d6b5
mmerioles <matthewmerioles@yahoo.com>
2026-05-19 12:32:12 -0700

    test cases

--- tests/test_modulus.py
+++ tests/test_modulus.py
@@ -2,16 +2,34 @@ import pytest
 
 from cpu.modulus import mod_multiply, mod_exp, fermat_inverse
 
-@pytest.mark.parametrize("a,b,p,expected", [
-    (10, 20, 100, 1000),
-    (1, 2, 100, 100)
+@pytest.fixture
+def prime():
+    return 17 #change this later to a large prime
+
+@pytest.mark.parametrize("a,b,expected", [
+    (2, 3, 6),
+    (4, 5, 3),
+    (16, 16, 1)
 ])
-def test_modular_multiplication(a, b, p, expected):
-    result = mod_multiply(a, b, p)
-    assert a * p == expected
+def test_modular_multiplication(a, b, expected, prime):
+    assert mod_multiply(a, b, prime) == expected
 
-def test_modular_exponentiation():
-    assert True
 
-def test_modular_inverse():
-    assert True
+@pytest.mark.parametrize("a,b,expected", [
+    (2, 3, 8),
+    (3, 4, 13),
+    (5, 0, 1),
+])
+def test_modular_exponentiation(a, b, expected, prime):
+    assert mod_exp(a, b, prime) == expected
+
+
+@pytest.mark.parametrize("a,expected", [
+    (1, 1),
+    (2, 9),
+    (3, 6),
+    (4, 13),
+    (5, 7),
+])
+def test_modular_inverse(a, expected, prime):
+    assert fermat_inverse(a, prime) == expected
