c5d355545020596a01d7088b4db731fd950d25e0
mmerioles <matthewmerioles@yahoo.com>
2026-05-19 13:15:43 -0700

    adjusting fixture

--- tests/test_modulus.py
+++ tests/test_modulus.py
@@ -2,34 +2,30 @@ import pytest
 
 from cpu.modulus import mod_multiply, mod_exp, fermat_inverse
 
-@pytest.fixture
-def prime():
-    return 17 #change this later to a large prime
-
-@pytest.mark.parametrize("a,b,expected", [
-    (2, 3, 6),
-    (4, 5, 3),
-    (16, 16, 1)
+@pytest.mark.parametrize("a,b,p,expected", [
+    (2, 3, 17, 6),
+    (4, 5, 17, 3),
+    (16, 16, 17, 1)
 ])
-def test_modular_multiplication(a, b, expected, prime):
-    assert mod_multiply(a, b, prime) == expected
+def test_modular_multiplication(a, b, p, expected):
+    assert mod_multiply(a, b, p) == expected
 
 
-@pytest.mark.parametrize("a,b,expected", [
-    (2, 3, 8),
-    (3, 4, 13),
-    (5, 0, 1),
+@pytest.mark.parametrize("a,b,p,expected", [
+    (2, 3, 17, 8),
+    (3, 4, 17, 13),
+    (5, 0, 17, 1),
 ])
-def test_modular_exponentiation(a, b, expected, prime):
-    assert mod_exp(a, b, prime) == expected
+def test_modular_exponentiation(a, b, p, expected):
+    assert mod_exp(a, b, p) == expected
 
 
-@pytest.mark.parametrize("a,expected", [
-    (1, 1),
-    (2, 9),
-    (3, 6),
-    (4, 13),
-    (5, 7),
+@pytest.mark.parametrize("a,p,expected", [
+    (1, 17, 1),
+    (2, 17, 9),
+    (3, 17, 6),
+    (4, 17, 13),
+    (5, 17, 7),
 ])
-def test_modular_inverse(a, expected, prime):
-    assert fermat_inverse(a, prime) == expected
+def test_modular_inverse(a, p, expected):
+    assert fermat_inverse(a, p) == expected
